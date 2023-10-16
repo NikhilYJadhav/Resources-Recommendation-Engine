960939eef8a7c4e9f7a01c999c659da366221138
Nikhil Jadhav <112622476+NikhilYJadhav@users.noreply.github.com>
2023-10-16 18:35:48 +0100

    Add files via upload

---
+++ private.py
@@ -0,0 +1,5 @@
+SEARCH_KEY = "AIzaSyBZjbORvnBVuBdo1q0dGL9BzmFppFu3iTU"
+SEARCH_ID = "8186630cb0a62444e"
+COUNTRY = "uk"
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
+RESULT_COUNT = 20
---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY = "AIzaSyBZjbORvnBVuBdo1q0dGL9BzmFppFu3iTU"
+SEARCH_ID = "8186630cb0a62444e"
+COUNTRY = "us"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
+RESULT_COUNT = 20
